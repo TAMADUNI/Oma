42f8edfd9337fea6bc07658522f31be197055bc4
IBUNHABIBU <ibunhabib@gmail.com>
2025-03-31 21:58:01 +0300

    Add shoulder matcher

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,13 @@ RSpec.configure do |config|
   config.include Devise::Test:IntegrationHelpers, type: :request
   config.include Devise::Test:Helpers, type: :view
 
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
