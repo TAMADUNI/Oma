447ef2c43695908b1be1c4828d76fca7c85293d5
IBUNHABIBU <ibunhabib@gmail.com>
2025-04-05 22:24:01 +0300

    Add devise testing env setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,7 +45,6 @@ RSpec.configure do |config|
   
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
-  
   config.include Devise::Test::ControllerHelpers, type: :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
