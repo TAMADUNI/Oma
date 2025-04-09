a03dd727a1ad4bbcaec0efe234961dcc2742b528
IBUNHABIBU <ibunhabib@gmail.com>
2025-04-09 22:56:22 +0300

    Configure devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,6 +19,7 @@ Devise.setup do |config|
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
+  
 # Hotwire 
 
   # ==> Mailer Configuration
