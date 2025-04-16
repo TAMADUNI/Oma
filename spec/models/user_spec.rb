8855dee2fa19be28fcced0243aeab3a674d6ea4a
IBUNHABIBU <ibunhabib@gmail.com>
2025-04-16 23:09:50 +0300

    Testing use spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
     describe 'validate' do 
+        
         it { should validate_presence_of(:email) }
         it { should validate_uniqueness_of(:email) }
         it { should allow_value('juma@example.com', juma.masudi@example.com).for(:email)}
         it { should validate_presence_of(:password)}
         it { should validate_length_of(:password).is_at_least(6) }
+
     end
   
 end
