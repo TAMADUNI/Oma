b836f441b62eb30b4e5375907f31bc747c5fca2c
IBUNHABIBU <ibunhabib@gmail.com>
2025-04-16 23:02:11 +0300

    Testing use spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
     describe 'validate' do 
-        it { should validate_presence_of(:name) }
+        it { should validate_presence_of(:email) }
+        it { should validate__of(:name) }
     end
   
 end
