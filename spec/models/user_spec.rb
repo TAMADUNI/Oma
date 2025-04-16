f8bfe6134b7d796fa20a16fadcd7f77877fb1219
IBUNHABIBU <ibunhabib@gmail.com>
2025-04-16 23:05:42 +0300

    Testing use spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     describe 'validate' do 
         it { should validate_presence_of(:email) }
         it { should validate_uniqueness_of(:email) }
+        it { should allow_value('juma@example.com', juma.masudi@example.com).for(:email)}
     end
   
 end
