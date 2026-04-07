cf4be37b0c9f7704b3f27bc43cae0c67b98a6b27
IBUNHABIBU <ibunhabib@gmail.com>
2026-04-07 21:18:24 +0300

    write test for devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Devise module" do 
+    it "includes database_authenticatable module" do
+      expect(User.ancestors).to include(Devise::Models::DatabaseAuthenticatable)
+    end
+    
+    it "includes registerable module" do
+      expect(User.ancestors).to include(Devise::Models::Registerable)
+    end
+    
+    it "includes recoverable module" do
+      expect(User.ancestors).to include(Devise::Models::Recoverable)
+    end
+
+    it "includes rememberable module" do
+      expect(User.ancestors).to include(Devise::Models::Rememberable)
+    end
+    
+    it "includes validatable module" do
+      expect(User.ancestors).to include(Devise::Models::Validatable)
+    end
+  end
 end
