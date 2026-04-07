5f5d1003034ebe7faf4421862d6877aee2315f41
IBUNHABIBU <ibunhabib@gmail.com>
2026-04-07 21:33:28 +0300

    Writing test for auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,11 +30,35 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password) }
     it { should validate_length_of(:password).is_at_least(6) }
+    it { should validate_presence_of(:role) }
+    it { should validate_inclusion_of(:role).in_array(%w[employee team_leader operation_manager hr_manager admin]) }
   end
 
     describe "associations" do
+    it { should belong_to(:supervisor).class_name('User').optional }
+    it { should have_many(:subordinates).class_name('User').with_foreign_key('supervisor_id') }
     it { should have_many(:task_logs).with_foreign_key('employee_id').dependent(:nullify) }
     it { should have_many(:behavior_scores).dependent(:destroy) }
     it { should have_many(:quality_issues).through(:task_logs) }
+    it { should have_many(:approved_task_logs).class_name('TaskLog').with_foreign_key('approver_id') }
   end
+  
+  describe "enums" do
+    it { should define_enum_for(:role).with_values(employee: 0, team_leader: 1, operation_manager: 2, hr_manager: 3, admin: 4) }
+  end
+  
+   describe "custom methods" do
+    let(:user) { create(:user) }
+    
+    describe "#employee?" do
+      it "returns true for employee role" do
+        user.employee!
+        expect(user.employee?).to be true
+      end
+      
+      it "returns false for admin role" do
+        user.admin!
+        expect(user.employee?).to be false
+      end
+    end
 end
