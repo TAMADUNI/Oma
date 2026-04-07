5f43ad23ba044b2f219abd9758070a45696b78c0
IBUNHABIBU <ibunhabib@gmail.com>
2026-04-07 23:42:28 +0300

    Add home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,10 @@
+# app/controllers/home_controller.rb
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  
+  def index
+    if user_signed_in?
+      redirect_to dashboard_path
+    end
+  end
 end
