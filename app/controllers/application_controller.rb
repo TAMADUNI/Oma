54147df973e58546f530aad81103771b049b8453
IBUNHABIBU <ibunhabib@gmail.com>
2026-04-07 23:49:18 +0300

    Add home controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,26 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  before_action :authenticate_user!, unless: :public_page?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :terms_of_service])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar])
+  end
+  
+  def public_page?
+    devise_controller? || 
+    root_path? ||
+    public_routes.include?(params[:action])
+  end
+  
+  def public_routes
+    ['about', 'contact', 'privacy', 'terms', 'products', 'solutions', 'resources', 'help']
+  end
+  
+  def root_path?
+    controller_name == 'home' && action_name == 'index'
+  end
 end
