bdfd9cda9357c7e596d64d7c01fee10de654644b
IBUNHABIBU <ibunhabib@gmail.com>
2026-04-14 17:23:17 +0300

    Fix the first user name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,6 @@
 # spec/factories/users.rb
 FactoryBot.define do
   factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
     email { Faker::Internet.unique.email }
     password { 'Password123!' }
     password_confirmation { 'Password123!' }
